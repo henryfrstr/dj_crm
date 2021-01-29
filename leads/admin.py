1e49691695edb5c1c6deb3e681fc1e704e82dfba
henryfrstr <63148122+henryfrstr@users.noreply.github.com>
2021-01-29 15:36:55 +0300

    added models to admin

--- leads/admin.py
+++ leads/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import User, Lead, Agent
 
+admin.site.register(User)
+admin.site.register(Lead)
+admin.site.register(Agent)
 # Register your models here.
